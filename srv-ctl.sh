c7efd8d645bf08336bab37c06021ff8edd9bba34
scienmind <scienmind@users.noreply.github.com>
2021-01-10 18:44:46 +0200

    adjust usage hint

--- srv-ctl.sh
+++ srv-ctl.sh
@@ -5,7 +5,7 @@ readonly SUCCESS=0
 readonly FAILURE=1
 
 function usage() {
-    echo "Usage:   $0  start | stop | unlock-only | stop-services-only | --help | -h"
+    echo "Usage:   $0  < start | stop | unlock-only | stop-services-only | help | -h >"
     return $FAILURE
 }
 
@@ -297,7 +297,7 @@ function main() {
     stop-services-only)
         stop_all_services
         ;;
-    --help)
+    help)
         usage
         ;;
     -h)
